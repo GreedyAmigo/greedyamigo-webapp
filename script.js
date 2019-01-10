e41f113625b858c3a238591e7c19bbf77c7da6e7
Manuel Fuchs <manuel.fuchs49@gmail.com>
2019-01-10 12:35:37 +0100

    remove obsolete file

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    // if (authorized() === true && windows.location.href.endsWith("index.html")) {
-    //     window.location.replace("/sites/dashboard");
-    // } else if (authorized() === false) {
-    //     window.location.replace("/index");
-    // }
-
-    //alert('ready');
-};
