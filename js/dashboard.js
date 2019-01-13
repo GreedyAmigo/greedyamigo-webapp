dd757f5ac186359a6ba846b8f39ed6084ba8ccc4
Manuel Fuchs <manuel.fuchs49@gmail.com>
2019-01-13 20:50:50 +0100

    add first dashboard version

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,53 +8,85 @@ redirectIfUnauthorized();
 
 Vue.use(VueApollo);
 
-let vueNavbar = new Vue({
-    el: "#navbar",
+let vueApplication = new Vue({
+    el: "#dashboard",
+    apolloProvider,
+    data: {
+        lendings: [
+            {
+                discriminator: "moneyLending",
+                id: 0,
+                dueDate: {
+                    day: 20,
+                    month: "Feb",
+                    year: 2019
+                },
+                description: "Washing machine chash",
+                cleared: false,
+                updatedAt: null,
+                createdAt: {
+                    day: 15,
+                    month: "Jan",
+                    year : 2015
+                },
+                amount: 15.4,
+                currency: {
+                    symbol: "€"
+                }
+            },
+            {
+                discriminator: "thingLending",
+                id: 0,
+                dueDate: {
+                    day: 20,
+                    month: "Sep",
+                    year: 2019
+                },
+                description: "Needed for a important thing",
+                cleared: true,
+                emoji: '👕',
+                updatedAt: null,
+                createdAt: {
+                    day: 15,
+                    month: "Jan",
+                    year : 2015
+                },
+                thing: {
+                    id: 5,
+                    label: "Washing machine"
+                }
+            }
+        ]
+    },
     methods: {
         logOutUser: function() {
             removeJwt();
             redirectIfUnauthorized();
         }
+        // logInUser: function() {
+        //     this.$apollo.query({
+        //         query:
+        //             gql`query ($email: String!, $password: String!) {
+        //                 login(email: $email, password: $password) {
+        //                     token
+        //                 }
+        //             }`,
+        //         variables: {
+        //             email:    this.email,
+        //             password: this.password
+        //         }
+        //     }).then((data) => {
+        //         this.errorMessage = "";
+
+        //         const token = data.data.login.token;
+        //         alert(token);
+
+        //         //todo: save token to local storage
+        //         //todo: redirect user
+        //     }).catch((error) => {
+        //         this.errorMessage = error.message;
+        //         console.error(error);
+        //     });
+        // }
     }
-})
-
-// let vueApplication = new Vue({
-//     el: "#dashboard",
-//     apolloProvider,
-//     data: {
-//         // email: "",
-//         // password: "",
-//         // errorMessage: ""
-//     },
-//     methods: {
-//         logOutUser: function() {
-//             removeJwt();
-//             redirectIfUnauthorized();
-//         }
-//         // logInUser: function() {
-//         //     this.$apollo.query({
-//         //         query:
-//         //             gql`query ($email: String!, $password: String!) {
-//         //                 login(email: $email, password: $password) {
-//         //                     token
-//         //                 }
-//         //             }`,
-//         //         variables: {
-//         //             email:    this.email,
-//         //             password: this.password
-//         //         }
-//         //     }).then((data) => {
-//         //         this.errorMessage = "";
-
-//         //         const token = data.data.login.token;
-//         //         alert(token);
-
-//         //         //todo: save token to local storage
-//         //         //todo: redirect user
-//         //     }).catch((error) => {
-//         //         this.errorMessage = error.message;
-//         //         console.error(error);
-//         //     });
-//         // }
-//     }
-// });
+});
